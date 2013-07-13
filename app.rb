fcdafbfb571d4d2340cc5362c1e8e571a97d5b76
Tushar Babbar <babbartushar@gmail.com>
2013-07-13 12:49:26 +0530

    bugs fixed

--- app.rb
+++ app.rb
@@ -1,20 +1,18 @@
 require 'sinatra'
 require './user'
 
-get '/register_user/:fb_id/:fb_access_token' do 
+get '/chat/register_user/:fb_id/:fb_access_token' do 
 	User.register_user(params["fb_id"],params["fb_access_token"])
 end
 
-get '/get_user_profile/:other_fb_id/:fb_id/:fb_access_token' do 
+get '/chat/get_user_profile/:other_fb_id/:fb_id/:fb_access_token' do 
 	User.get_user_profile(params["fb_id"],params["fb_id"],params["fb_access_token"])
 end
 
-get '/get_user_photos/:other_fb_id/:fb_id/:fb_access_token' do 
+get '/chat/get_user_photos/:other_fb_id/:fb_id/:fb_access_token' do 
 	User.get_user_photos(params["fb_id"],params["fb_id"],params["fb_access_token"])
-get '/get_user_movies/:other_fb_id/:fb_id/:fb_access_token' do 
-	User.get_user_movies(params["fb_id"],params["fb_id"],params["fb_access_token"])	
 end
 
-get '/get_user_movies/:other_fb_id/:fb_id/:fb_access_token' do 
+get '/chat/get_user_movies/:other_fb_id/:fb_id/:fb_access_token' do 
 	User.get_user_movies(params["fb_id"],params["fb_id"],params["fb_access_token"])	
 end
