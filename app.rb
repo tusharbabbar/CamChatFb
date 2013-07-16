53c370b6ec4235b5ae816933954a98933a13a2c8
Tushar Babbar <babbartushar@gmail.com>
2013-07-16 10:58:53 +0530

    bug fixed in app.rb

--- app.rb
+++ app.rb
@@ -10,20 +10,20 @@ post '/chat/user_about' do
 end
 
 get '/chat/get_user_profile/:other_fb_id/:fb_id/:fb_access_token' do 
-	User.get_user_profile(params["fb_id"],params["fb_id"],params["fb_access_token"])
+	User.get_user_profile(params["other_fb_id"],params["fb_id"],params["fb_access_token"])
 end
 
 get '/chat/get_user_about/:other_fb_id/:fb_id/:fb_access_token' do 
-	User.get_user_about(params["fb_id"],params["fb_id"],params["fb_access_token"])
+	User.get_user_about(params["other_fb_id"],params["fb_id"],params["fb_access_token"])
 end
 
 get '/chat/get_user_photos/:other_fb_id/:fb_id/:fb_access_token' do 
-	User.get_user_photos(params["fb_id"],params["fb_id"],params["fb_access_token"])
+	User.get_user_photos(params["other_fb_id"],params["fb_id"],params["fb_access_token"])
 end
 
 get '/chat/get_user_movies/:other_fb_id/:fb_id/:fb_access_token' do 
-	User.get_user_movies(params["fb_id"],params["fb_id"],params["fb_access_token"])
+	User.get_user_movies(params["other_fb_id"],params["fb_id"],params["fb_access_token"])
 end
 get '/chat/get_user_music/:other_fb_id/:fb_id/:fb_access_token' do 
-	User.get_user_music(params["fb_id"],params["fb_id"],params["fb_access_token"])	
+	User.get_user_music(params["other_fb_id"],params["fb_id"],params["fb_access_token"])	
 end
